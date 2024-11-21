0306469d91e04852da8a2c7d3d91df32ba5cf1e3
hieudaok63 <hieudaok63@gmail.com>
2024-11-21 09:30:55 +0700

    logic login

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,39 @@
+import authApiRequest from "@/apiRequest/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as LoginBodyType;
+  const cookieStore = await cookies();
+  console.log(1);
+  try {
+    const { payload } = await authApiRequest.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    console.log(2);
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    cookieStore.set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedAccessToken.exp * 1000,
+    });
+    cookieStore.set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, { status: error.status });
+    } else {
+      return Response.json({ message: "Loi server" }, { status: 500 });
+    }
+  }
+}
